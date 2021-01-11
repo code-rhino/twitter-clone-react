f79de6dce6bc31a54ce4a8d497c7aa698118408e
Tariq Hook <thook@Tariqs-MacBook-Pro.local>
2021-01-11 16:34:27 -0500

    first checkin

---
+++ src/TweetBox.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import "./TweetBox.css";
+import { Avatar, Button } from "@material-ui/core";
+import db from "./firebase";
+
+function TweetBox() {
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    e.preventDefault();
+
+    db.collection("posts").add({
+      displayName: "Rafeh Qazi",
+      username: "cleverqazi",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar:
+        "https://avatars1.githubusercontent.com/u/1588674?s=460&u=49f5c65918d7ab0050721f0ba02cb5a32019ef97&v=4",
+    });
+
+    setTweetMessage("");
+    setTweetImage("");
+  };
+
+  return (
+    <div className="tweetBox">
+      <form>
+        <div className="tweetBox__input">
+          <Avatar src="https://avatars1.githubusercontent.com/u/1588674?s=460&u=49f5c65918d7ab0050721f0ba02cb5a32019ef97&v=4" />
+          <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+            placeholder="What's happening?"
+            type="text"
+          />
+        </div>
+        <input
+          value={tweetImage}
+          onChange={(e) => setTweetImage(e.target.value)}
+          className="tweetBox__imageInput"
+          placeholder="Optional: Enter image URL"
+          type="text"
+        />
+
+        <Button
+          onClick={sendTweet}
+          type="submit"
+          className="tweetBox__tweetButton"
+        >
+          Tweet
+        </Button>
+      </form>
+    </div>
+  );
+}
+
+export default TweetBox;
